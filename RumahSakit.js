7cba59c02a519f160fbebdf77d0183a8c2206161
riskadeva <riskadeva14@gmail.com>
2026-05-13 17:23:24 +0700

    tenagamedis

---
+++ RumahSakit.js
@@ -0,0 +1,59 @@
+
+class WhatsAppNotification {
+    sendMessage() {
+        console.log("Kirim notifikasi WhatsApp");
+    }
+}
+
+class AppointmentService {
+    constructor() {
+        this.notification = new WhatsAppNotification();
+    }
+    bookAppointment() {
+        console.log("Appointment berhasil");
+        this.notification.sendMessage();
+    }
+}
+
+class CashPayment {
+    pay() {
+        console.log("Bayar Tunai");
+    }
+}
+
+class PaymentService {
+    constructor() {
+        this.payment = new CashPayment();
+    }
+    processPayment() {
+        this.payment.pay();
+    }
+}
+
+class MySQLDatabase {
+    connect() {
+        console.log("Connect MySQL");
+    }
+}
+
+class MedicalRecordService {
+    constructor() {
+        this.db = new MySQLDatabase();
+    }
+    saveRecord() {
+        this.db.connect();
+        console.log("Data rekam medis disimpan");
+    }
+}
+
+console.log("=== SISTEM APPOINTMENT ===");
+const appointmentService = new AppointmentService();
+appointmentService.bookAppointment();
+
+console.log("\n=== SISTEM PEMBAYARAN ===");
+const paymentService = new PaymentService();
+paymentService.processPayment();
+
+console.log("\n=== SISTEM REKAM MEDIS ===");
+const medicalRecordService = new MedicalRecordService();
+medicalRecordService.saveRecord();
